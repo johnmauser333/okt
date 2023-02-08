679b0d9c2cce0d5897b2fe9b3d3a2151fff15d3e
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2023-02-08 23:36:56 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,6 @@ PORT=10000
 # 伪装 xray 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv xray ${RELEASE_RANDOMNESS}
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 cat config.json | base64 > config
 rm -f config.json
 
